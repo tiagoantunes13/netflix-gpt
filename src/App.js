677242273d0a6628fc7083ad615e25a07deb5f66
Tiago Antunes <tiagoantunes9991@gmail.com>
2025-07-26 23:16:05 +0100

    header and browser and router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold underline">
-      <p>NETFLIX GPT</p>
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="absolute top-0 left-0 z-10 w-full px-8 py-6 bg-gradient-to-b from-black">
+      <img
+        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-07-24/consent/87b6a5c0-0104-4e96-a291-092c11350111/019808e2-d1a7-75a7-a8b7-f85133e4c823/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="Netflix"
+        className="w-44"
+      />
+    </div>
+  );
+};
+export default Header;
