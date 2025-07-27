f248f45b55c182c7a057d170252fc56506f7212f
Tiago Antunes <tiagoantunes9991@gmail.com>
2025-07-27 13:55:32 +0100

    add firebase to my project

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9nQ-_oUr1f8-MStOy1WHd2IKbhC9FheY",
+  authDomain: "netflixgpt-7e5b7.firebaseapp.com",
+  projectId: "netflixgpt-7e5b7",
+  storageBucket: "netflixgpt-7e5b7.firebasestorage.app",
+  messagingSenderId: "200932540170",
+  appId: "1:200932540170:web:1f532ebf4bee8108b284f3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
